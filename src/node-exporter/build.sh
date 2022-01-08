b54f82deb955e541eeeacf8f6257684ea0b32d2a
Vitaliy Vasilenko <me@vitalvas.com>
2022-01-08 05:20:32 +0200

    update build node-exporter

--- src/node-exporter/build.sh
+++ src/node-exporter/build.sh
@@ -2,7 +2,5 @@
 
 VERSION='1.3.1'
 
-wget https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz
+wget -q https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz
 tar --strip-components 1 -xvf node_exporter-${VERSION}.linux-amd64.tar.gz
-
-ls -lah .
