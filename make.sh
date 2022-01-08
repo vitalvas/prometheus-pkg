41d3ef93969af24e4251cb52e197417f14a949de
Vitaliy Vasilenko <me@vitalvas.com>
2022-01-08 04:18:36 +0200

    add build packages

--- make.sh
+++ make.sh
@@ -4,11 +4,26 @@ set -e -o pipefail
 
 APP=$1
 
+SRC="src/${APP}"
+
+function install_nfpm {
+    echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | tee /etc/apt/sources.list.d/goreleaser.list
+    apt update
+    apt install -qy nfpm
+}
+
 if [ -z "${APP}" ]; then
     echo 'No APP defined'
     exit 1
 fi
 
-if [ -f "src/${APP}/build.sh" ]; then
+[ -f "${SRC}/build.sh" ] || {
     bash -x src/${APP}/build.sh
-fi
+}
+
+[ -f "${SRC}/nfpm.yaml" ] || {
+    install_nfpm
+    nfpm pkg --packager deb 
+}
+
+ls -lah .
