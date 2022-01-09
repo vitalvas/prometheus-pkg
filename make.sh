2f378239541da59d6c08ec2939c5302350910e3e
Vitaliy Vasilenko <me@vitalvas.com>
2022-01-09 07:09:36 +0200

    move to actions install nfpm and build

--- make.sh
+++ make.sh
@@ -6,23 +6,13 @@ APP=$1
 
 SRC="src/${APP}"
 
-function install_nfpm {
-    echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
-    sudo apt update
-    sudo apt install -qy nfpm
-}
 
 if [ -z "${APP}" ]; then
     echo 'No APP defined'
     exit 1
 fi
 
-[ -f "${SRC}/build.sh" ] && {
-    bash -x ${SRC}/build.sh
-}
-
 [ -f "${SRC}/nfpm.yaml" ] && {
-    install_nfpm
     nfpm pkg --packager deb -f ${SRC}/nfpm.yaml
 }
 
