118676f3d777cd2b3e63772018bcdfb1ffa9d1d5
Vitaliy Vasilenko <me@vitalvas.com>
2022-05-31 18:10:39 +0300

    fix comand order

--- tools/scripts/postinstall.sh
+++ tools/scripts/postinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+grep -q prometheus /etc/group || groupadd --system prometheus
+grep -q prometheus /etc/shadow || useradd -s /sbin/nologin --system -g prometheus prometheus
+
 if command -V systemctl >/dev/null 2>&1; then
     systemctl daemon-reload ||:
     systemctl unmask __name__ ||:
@@ -7,6 +10,3 @@ if command -V systemctl >/dev/null 2>&1; then
     systemctl enable __name__ ||:
     systemctl restart __name__ ||:
 fi
-
-grep -q prometheus /etc/group || groupadd --system prometheus
-grep -q prometheus /etc/shadow || useradd -s /sbin/nologin --system -g prometheus prometheus
