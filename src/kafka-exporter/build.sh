c3dd8fc9e8e17b61076e3dcbf5f4a43769b5c201
Vitaliy Vasilenko <me@vitalvas.com>
2022-06-01 18:55:31 +0300

    add kafka exporter

---
+++ src/kafka-exporter/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION='1.4.0'
+
+wget -q https://github.com/danielqsj/kafka_exporter/releases/download/v${VERSION}/kafka_exporter-${VERSION}.linux-amd64.tar.gz
+tar --strip-components 1 -xvf kafka_exporter-${VERSION}.linux-amd64.tar.gz
+
+cat ../../tools/scripts/postinstall.sh | sed 's/__name__/kafka-exporter/g' > postinstall.sh
