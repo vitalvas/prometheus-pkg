ecf6bba90c7e30e1de69351296f88d34cedd555a
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 18:28:36 -0400

    feat(kafka-exporter): update

--- src/kafka-exporter/build.sh
+++ src/kafka-exporter/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION='1.4.0'
+VERSION='1.7.0'
 
 wget -q https://github.com/danielqsj/kafka_exporter/releases/download/v${VERSION}/kafka_exporter-${VERSION}.linux-amd64.tar.gz
 tar --strip-components 1 -xvf kafka_exporter-${VERSION}.linux-amd64.tar.gz
