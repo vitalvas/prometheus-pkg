9728d5d5dad7693e88518641c475f12411b8271c
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 16:41:30 -0400

    feat(node-exporter): update

--- src/node-exporter/build.sh
+++ src/node-exporter/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION='1.3.1'
+VERSION='1.6.1'
 
 wget -q https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz
 tar --strip-components 1 -xvf node_exporter-${VERSION}.linux-amd64.tar.gz
