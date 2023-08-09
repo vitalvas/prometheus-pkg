8953f8004f218a6ad050738cd6fb73dcf9f6175a
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 18:25:40 -0400

    feat(blackbox-exporter): update

--- src/blackbox-exporter/build.sh
+++ src/blackbox-exporter/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION='0.21.0'
+VERSION='0.24.0'
 
 wget -q https://github.com/prometheus/blackbox_exporter/releases/download/v${VERSION}/blackbox_exporter-${VERSION}.linux-amd64.tar.gz
 tar --strip-components 1 -xvf blackbox_exporter-${VERSION}.linux-amd64.tar.gz
