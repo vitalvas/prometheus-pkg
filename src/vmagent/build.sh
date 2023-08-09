ea7e5ab1877b0e11edb5623f7e4ae6e3eebc4a59
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 18:37:13 -0400

    feat(vmagent): update

--- src/vmagent/build.sh
+++ src/vmagent/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION='1.77.2'
+VERSION='1.92.1'
 
-wget -q https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v${VERSION}/vmutils-amd64-v${VERSION}.tar.gz
-tar -xvf vmutils-amd64-v${VERSION}.tar.gz 
+wget -q https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v${VERSION}/vmutils-linux-amd64-v${VERSION}.tar.gz
+tar -xvf vmutils-linux-amd64-v${VERSION}.tar.gz 
 
 cat ../../tools/scripts/postinstall.sh | sed 's/__name__/vmagent/g' > postinstall.sh
