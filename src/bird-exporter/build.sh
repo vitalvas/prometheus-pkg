9f304b0a743042e35e570095eb8c2678a9a564d2
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 18:46:20 -0400

    fix(bird-exporter): init

--- src/bird-exporter/build.sh
+++ src/bird-exporter/build.sh
@@ -3,6 +3,6 @@
 VERSION='1.4.1'
 
 wget -q https://github.com/czerwonk/bird_exporter/releases/download/${VERSION}/bird_exporter_${VERSION}_linux_amd64.tar.gz
-tar --strip-components 1 -xvf bird_exporter_${VERSION}_linux_amd64.tar.gz
+tar -xvf bird_exporter_${VERSION}_linux_amd64.tar.gz
 
 cat ../../tools/scripts/postinstall.sh | sed 's/__name__/bird-exporter/g' > postinstall.sh
