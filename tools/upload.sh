a6db6ec39c12fcf119f88135a082e8a369237c03
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 23:10:05 -0400

    chore: remove legacy

--- tools/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for NAME in $(ls *.deb); do
-    curl --user ${REPO_USER_NAME}:${REPO_USER_PASS} \
-        --silent --fail --upload-file ${NAME} \
-        --retry 5 --retry-delay 0 --retry-max-time 40 \
-        https://${REPO_UPLOAD_ADDR}/prometheus/ubuntu/common/main/${NAME}
-done
