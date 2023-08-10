121c29156ba5783d81db018cfb3d90287f918bec
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 22:29:23 -0400

    feat(blackbox-exporter): move to new repo

--- src/blackbox-exporter/build.sh
+++ src/blackbox-exporter/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -e
+
 VERSION='0.24.0'
+ARCHS=(amd64 arm64)
 
-wget -q https://github.com/prometheus/blackbox_exporter/releases/download/v${VERSION}/blackbox_exporter-${VERSION}.linux-amd64.tar.gz
-tar --strip-components 1 -xvf blackbox_exporter-${VERSION}.linux-amd64.tar.gz
+for ARCH in ${ARCHS[@]}; do
+    rm -Rf tmp/
+    mkdir -p tmp/
 
-cat ../../tools/scripts/postinstall.sh | sed 's/__name__/blackbox-exporter/g' > postinstall.sh
+    wget -q https://github.com/prometheus/blackbox_exporter/releases/download/v${VERSION}/blackbox_exporter-${VERSION}.linux-${ARCH}.tar.gz
+    tar --strip-components 1 -xvf blackbox_exporter-${VERSION}.linux-${ARCH}.tar.gz -C tmp/
+
+    env PKG_ARCH=${ARCH} PKG_VERSION=${VERSION} nfpm pkg --packager deb -f nfpm.yaml
+done
