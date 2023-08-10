f726647af9597dd187e20979cd3fde19caed7255
Vitaliy Vasilenko <me@vitalvas.com>
2023-08-09 23:03:23 -0400

    feat(postgres-exporter): move to new repo

--- src/postgres-exporter/build.sh
+++ src/postgres-exporter/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -e
+
 VERSION='0.13.2'
+ARCHS=(amd64 arm64)
 
-wget -q https://github.com/prometheus-community/postgres_exporter/releases/download/v${VERSION}/postgres_exporter-${VERSION}.linux-amd64.tar.gz
-tar --strip-components 1 -xvf postgres_exporter-${VERSION}.linux-amd64.tar.gz
+for ARCH in ${ARCHS[@]}; do
+    rm -Rf tmp/
+    mkdir -p tmp/
 
-cat ../../tools/scripts/postinstall.sh | sed 's/__name__/postgres-exporter/g' > postinstall.sh
+    wget -q https://github.com/prometheus-community/postgres_exporter/releases/download/v${VERSION}/postgres_exporter-${VERSION}.linux-${ARCH}.tar.gz
+    tar --strip-components 1 -xvf postgres_exporter-${VERSION}.linux-${ARCH}.tar.gz
+
+    env PKG_ARCH=${ARCH} PKG_VERSION=${VERSION} nfpm pkg --packager deb -f nfpm.yaml
+done
